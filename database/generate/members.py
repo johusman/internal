b3157f646cd44e75a5dfbfe68706a59c871793f5
Johan Husman <johusman@gmail.com>
2015-07-04 18:40:38 +0200

    Generate test data: moved to phone numbers with a +46 prefix, and added male first names (and civil reg numbers).

--- database/generate/members.py
+++ database/generate/members.py
@@ -7,11 +7,11 @@ import random
 # Collisions are so unlikely that we don't care enough to check for them
 # Real "personnummer" can have a + instead of a -, indicating that the person is over 100 years old,
 # but since we use four digits for the year, we ignore this and just use -.
-def make_civic_regno(isSamordningsnummer):
+def make_civic_regno(is_female, is_samordningsnummer):
     birthdatetime = common.get_random_datetime(datetime.datetime(1900,1,1), datetime.datetime(2010,1,1))
-    birthdate = "%04d%02d%02d" % (birthdatetime.year, birthdatetime.month, birthdatetime.day + (60 if isSamordningsnummer else 0)) 
+    birthdate = "%04d%02d%02d" % (birthdatetime.year, birthdatetime.month, birthdatetime.day + (60 if is_samordningsnummer else 0)) 
     short_birthdate = birthdate[-6:]
-    birth_number = "%03d" % (int(random.random() * 500)*2)
+    birth_number = "%03d" % (int(random.random() * 500)*2 + (1 if not is_female else 0))
 
     checksum_string = short_birthdate + birth_number
     odd = False
@@ -28,9 +28,9 @@ def make_civic_regno(isSamordningsnummer):
 
 def make_phone_number():
     if random.random() > 0.5:
-        prefix = "08"
+        prefix = "+468"
     else:
-        prefix = "073"
+        prefix = "+4673"
 
     number = prefix
     for i in range(1, 8):
@@ -50,8 +50,10 @@ class Member:
     phone = None
 
 def generate(db, count):
-    with open("firstnames.txt") as f:
-        firstnames = f.readlines()
+    with open("firstnames_female.txt") as f:
+        firstnames_female = f.readlines()
+    with open("firstnames_male.txt") as f:
+        firstnames_male = f.readlines()
     with open("lastnames.txt") as f:
         lastnames = f.readlines()
 
@@ -62,13 +64,18 @@ def generate(db, count):
 
     generated_ids = []
     for i in range(1, count+1):
+        is_female = random.random() < 0.5
+
         member = Member()
         member.created = common.get_random_datetime(datetime.datetime(2013,1,1), datetime.datetime.now())
         member.updated = common.get_random_datetime(datetime.datetime(2013,1,1), datetime.datetime.now())
         member.email = common.get_short_unique_string() + '@test.makerspace.se'
-        member.firstname = firstnames[int(random.random()*len(firstnames))].strip()
+        if is_female:
+            member.firstname = firstnames_female[int(random.random()*len(firstnames_female))].strip()
+        else:
+            member.firstname = firstnames_male[int(random.random()*len(firstnames_male))].strip()
         member.lastname = lastnames[int(random.random()*len(lastnames))].strip()
-        member.civicregno = make_civic_regno(i == samordning_index or random.random() < samordning_frequency)
+        member.civicregno = make_civic_regno(is_female, i == samordning_index or random.random() < samordning_frequency)
         member.country = 'SE'
         member.phone = make_phone_number()
         try:
