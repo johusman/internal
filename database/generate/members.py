3bc808843eaac2ea130e736fb8d7e0323f01464c
Johan Husman <johusman@gmail.com>
2015-07-04 18:19:53 +0200

    Generate test data: some "personnummer" should be "samordningsnummer".

--- database/generate/members.py
+++ database/generate/members.py
@@ -5,9 +5,11 @@ import datetime
 import random
 
 # Collisions are so unlikely that we don't care enough to check for them
-def make_civic_regno():
+# Real "personnummer" can have a + instead of a -, indicating that the person is over 100 years old,
+# but since we use four digits for the year, we ignore this and just use -.
+def make_civic_regno(isSamordningsnummer):
     birthdatetime = common.get_random_datetime(datetime.datetime(1900,1,1), datetime.datetime(2010,1,1))
-    birthdate = "%04d%02d%02d" % (birthdatetime.year, birthdatetime.month, birthdatetime.day) 
+    birthdate = "%04d%02d%02d" % (birthdatetime.year, birthdatetime.month, birthdatetime.day + (60 if isSamordningsnummer else 0)) 
     short_birthdate = birthdate[-6:]
     birth_number = "%03d" % (int(random.random() * 500)*2)
 
@@ -53,6 +55,11 @@ def generate(db, count):
     with open("lastnames.txt") as f:
         lastnames = f.readlines()
 
+    # At least one member per call should use a "samordningsnummer", and also about one in a hundred randomly
+    # http://www.skatteverket.se/privat/sjalvservice/blanketterbroschyrer/broschyrer/info/707.4.39f16f103821c58f680007997.html
+    samordning_index = int(random.random() * count)
+    samordning_frequency = 0.01
+
     generated_ids = []
     for i in range(1, count+1):
         member = Member()
@@ -61,7 +68,7 @@ def generate(db, count):
         member.email = common.get_short_unique_string() + '@test.makerspace.se'
         member.firstname = firstnames[int(random.random()*len(firstnames))].strip()
         member.lastname = lastnames[int(random.random()*len(lastnames))].strip()
-        member.civicregno = make_civic_regno()
+        member.civicregno = make_civic_regno(i == samordning_index or random.random() < samordning_frequency)
         member.country = 'SE'
         member.phone = make_phone_number()
         try:
